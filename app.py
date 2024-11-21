b454116bc0447cde3f7c5fd3523c9fa8a3011620
a gib <arielegibson@gmail.com>
2024-11-21 13:57:47 +0000

    monitor

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from data.database import initialise_database, add_order, clear_orders, count_or
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
+from azure.monitor.opentelemetry import configure_azure_monitor
 import logging
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -16,6 +18,7 @@ initialise_scheduled_jobs(app)
 
 
 logging.basicConfig(level=logging.INFO)
+configure_azure_monitor()
 
 
 @app.route("/")
